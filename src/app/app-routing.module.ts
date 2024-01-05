faf852454c20ec4c52a7fe93c8988c207fd1decb
Gabriel Resende <resendepiece@gmail.com>
2024-01-05 10:50:04 -0300

    finalizando a parte home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { BurgersComponent } from './views/burgers/burgers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "burguers", component: BurgersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
